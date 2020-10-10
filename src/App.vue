f0125131deb9ba3c5560f04c52dfe4799d27fcb9
Takunda <takuchirema@yahoo.com>
2020-10-10 06:44:50 +0200

    dashboard stuff and more

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,7 @@
     },
     data() {
       return {
+        loading: true,
         menu: [
           {
             header: true,
@@ -66,6 +67,28 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin: 20px;
+    margin-left: 30px;
+  }
+
+  .loader {  /* Loader Div Class */
+    justify-content: center;
+    position: absolute;
+    top:0px;
+    right:0px;
+    width:100%;
+    height:100%;
+    background-color:#300;
+    background-size: 50px;
+    background-repeat:no-repeat;
+    background-position:center;
+    z-index:10000000;
+    opacity: 0.8;
+    filter: alpha(opacity=40);
+  }
+
+  .loader div {
+    justify-content: center;
+    display: inline-block;
+    margin-top: 20%;
   }
 </style>
