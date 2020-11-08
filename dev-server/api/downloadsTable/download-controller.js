99469852007c88f9b42fb93deab11d77a2eb9593
Miguel-P <miguelfidelpereira@gmail.com>
2020-11-08 15:50:23 +0200

    Intial scaffolding for adding synthesised beta tables to web server. Part 1/2

--- dev-server/api/downloadsTable/download-controller.js
+++ dev-server/api/downloadsTable/download-controller.js
@@ -3,6 +3,9 @@ import {sequelize} from '../config/db';
 const { Op } = require("sequelize");
 
 var FullBetaModel = require('../../model/full-beta-model').default;
+var PortfolioMetrics = require('../../model/portfolio-metrics-model').default;
+var IndustryPortfolioMetrics = require('../../model/industry-portf-model').default;
+var SharesMetrics = require('../../model/shares-metrics-model').default;
 const allIndexNames = require('../utilities/index-names')
 
 export function get_all(req, res) {
@@ -23,7 +26,7 @@ export function get_indextable(req, res) {
     var period = req.params["period"]
     FullBetaModel.findAll({
         where: sequelize.literal(
-            " \"Instrument\" NOT IN (:indexes) AND"+
+            " \"Instrument\" IN (:indexes) AND"+
             " \"Index\" = '" + proxy + "' AND" +
             " (Date BETWEEN DATEADD(MONTH,"+(-1 * period)+",'"+date+"') AND '"+date+"')"
         ),
@@ -61,6 +64,37 @@ export function get_dates(req, res) {
     FullBetaModel.sequelize.query('SELECT distinct "Date" from tbl_BA_Beta_Output order by "Date" desc', {
             type: FullBetaModel.sequelize.QueryTypes.SELECT
     })
+    .then(function(results) {
+        return res.status(200).json({message: results});
+    }).catch(function (err) {
+        return res.status(400).json({ error: err.message});
+    });
+}
+
+// Waiting on one or two pieces of info before I continue with this.
+export function get_all_SharesMetrics(req, res) {
+    // First find most recent date in the constituents
+    SharesMetrics.findAll()
+    .then(function(results) {
+        return res.status(200).json({message: results});
+    }).catch(function (err) {
+        return res.status(400).json({ error: err.message});
+    });
+}
+
+export function get_all_IndusPortfMetrics(req, res) {
+    // First find most recent date in the constituents
+    IndustryPortfolioMetrics.findAll()
+    .then(function(results) {
+        return res.status(200).json({message: results});
+    }).catch(function (err) {
+        return res.status(400).json({ error: err.message});
+    });
+}
+
+export function get_all_PortfMetrics(req, res) {
+    // First find most recent date in the constituents
+    PortfolioMetrics.findAll()
     .then(function(results) {
         return res.status(200).json({message: results});
     }).catch(function (err) {
--- dev-server/api/downloadsTable/download-routes.js
+++ dev-server/api/downloadsTable/download-routes.js
@@ -5,6 +5,11 @@ import * as controller from './download-controller';
 // The controller task will perform the action
 router.get('/downloads',controller.get_all);
 
+// TODO: complete routes once data is understood.
+router.get('/downloads/sharesMetrics',controller.get_all_SharesMetrics);
+router.get('/downloads/portfMetrics',controller.get_all_PortfMetrics);
+router.get('/downloads/indusPortfMetrics',controller.get_all_IndusPortfMetrics);
+
 router.get('/downloads/dates',controller.get_dates);
 
 router.get('/downloads/indextable/:proxy/date/:date/period/:period',controller.get_indextable);
