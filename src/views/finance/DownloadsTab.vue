a36f49a839d8a30694659a657b3f4b9bf339927c
Miguel-P <miguelfidelpereira@gmail.com>
2020-11-14 16:47:48 +0200

    Change from model-select component to standard vue-bootstrap b-form-select component so that options can be cleared when table type is changed. Add more padding to the different selectors.

--- src/views/finance/DownloadsTab.vue
+++ src/views/finance/DownloadsTab.vue
@@ -9,50 +9,51 @@
             <div class="horizontal-no-margin">
                     <div class="horizontal w100">
                         <div class="w20">
-                            <model-select
+                            <b-form-select
+                                disabled-field="notEnabled"
                                 :options="tables"
                                 v-model="table"
                                 placeholder="Select Table">
-                            </model-select>
+                            </b-form-select>
                         </div>
-                        <div class="w20" v-if="index_and_share_codes_req">  
-                        <model-select 
-                                :options="index_codes"
-                                v-model="index_code"
-                                placeholder="Select Index Code">
-                            </model-select>
+                        <div class="w20" v-if="index_codes_req">  
+                            <b-form-select
+                                    :options="index_codes"
+                                    v-model="index_code"
+                                    placeholder="Select Index Code">
+                            </b-form-select>
                         </div>
                         <div class="w20">  
-                            <model-select 
+                            <b-form-select 
                                 :options="marketProxies"
                                 v-model="marketProxy"
                                 placeholder="Select Market Proxy">
-                            </model-select>
+                            </b-form-select>
                         </div>
-                        <div class="w20" v-if="index_and_share_codes_req"> 
-                            <model-select 
+                        <div class="w20" v-if="share_codes_req"> 
+                            <b-form-select 
                                 :options="share_codes"
                                 v-model="share_code"
                                 placeholder="Select Share Code">
-                            </model-select>
+                            </b-form-select>
                         </div>
                         <div class="w20">
-                            <model-select 
+                            <b-form-select 
                                 :options="dates"
                                 v-model="date"
                                 placeholder="Select Date">
-                            </model-select>
+                            </b-form-select>
                         </div>
 
                         <div class="w20">
-                            <model-select 
+                            <b-form-select 
                                 :options="periods"
                                 v-model="period"
                                 placeholder="Select Period">
-                            </model-select>
+                            </b-form-select>
                         </div>
                     </div>
-                    <div class="w100">
+                    <div class="w100 margined">
                         <div class="w20">
                             <input type="button" value="Create Table"  @click="createTable()" class="btn btn-info w60"/>
                         </div>
@@ -61,7 +62,7 @@
                                 <input type="button" value="Download Table"  @click="downloadTable()" class="btn btn-info w60"/>
                         </div>
                     </div>
-                    <b-form-group label="Add/Remove Columns:" v-show='loaded'>
+                    <b-form-group class = "margined" label="Add/Remove Columns:" v-show='loaded'>
                         <b-form-checkbox-group
                           id="checkbox-group-1"
                           v-model="selected_columns"
@@ -256,7 +257,7 @@
             // get the index codes
             axios.get('http://localhost:3000/api/downloads/index_codes')
             .then(response => {
-                console.log("Fetching share codes")
+                console.log("Fetching index codes")
                 // console.log("response", response)
                 var indexCodesArray = response.data["message"]
                 for (var i=0; i <indexCodesArray.length; i++) {
@@ -275,38 +276,48 @@
                 results: {},
                 loaded: false,
                 tables: [
+                    {value: null, text: 'Please Select Table' , notEnabled: true},
                     {value: 'sharetable', text: 'Share Table'},
                     {value: 'indextable', text: 'Index Table'},
                     {value: 'indusPortfMetrics', text: 'Industry Portfolio Table'},
                     {value: 'sharesMetrics', text: 'Share Metrics Table'},
                     {value: 'portfMetrics', text: 'Portfolio Metrics Table'}
                 ],
-                table: '',
+                table: null,
                 marketProxies: [
+                    {value: null, text: 'Please Select Market Proxy', notEnabled: true },
                     {value: 'J200', text: 'J200'},
                     {value: 'J203', text: 'J203'},
                     {value: 'J250', text: 'J250'},
                     {value: 'J257', text: 'J257'},
                     {value: 'J258', text: 'J258'}
                 ],
-                marketProxy: '',
-                dates: [],
-                date: '',
+                marketProxy: null,
+                dates: [
+                    {value: null, text: 'Please Select Date', notEnabled: true }
+                ],
+                date: null,
                 periods: [
+                    {value: null, text: 'Please Select Period', notEnabled: true},
                     {value: '1', text: '1 month'},
                     {value: '6', text: '6 months'},
                     {value: '12', text: '1 year'},
                     {value: '60', text: '5 years'},
                     {value: '120', text: '10 years'},
                 ],
-                period: '',
-                share_codes: [],
-                share_code: '',
-                index_codes: [],
-                index_code: '',
+                period: null,
+                share_codes: [
+                    {value: null, text: 'Please Select Share Code', notEnabled: true},
+                ],
+                share_code: null,
+                index_codes: [
+                    {value: null, text: 'Please Select Index Code', notEnabled: true},
+                ],
+                index_code: null,
                 selected_columns: [],
                 possible_columns: [],
-                index_and_share_codes_req: false
+                share_codes_req: false,
+                index_codes_req: false
             }
         },
         watch: {
@@ -320,14 +331,27 @@
                 }
             },
             table: function () {
-                if (this.table == "sharesMetrics" || this.table == "indusPortfMetrics") {
-                    this.index_and_share_codes_req = true
-                    this.marketProxy = ''
-                    this.date = ''
-                    this.period = ''
+                if (this.table == "sharesMetrics") {
+                    this.share_codes_req = true
+                    this.index_codes_req = true
+                    this.marketProxy = null
+                    this.date = null
+                    this.share_code = null
+                    this.index_code = null
+                    this.period = null
+                }
+                else if (this.table == "indusPortfMetrics") {
+                    this.share_codes_req = false
+                    this.index_codes_req = true
+                    this.marketProxy = null
+                    this.date = null
+                    this.share_code = null
+                    this.index_code = null
+                    this.period = null
                 }
                 else {
-                    this.index_and_share_codes_req = false
+                    this.share_codes_req = false
+                    this.index_codes_req = false
                 }
             }
         }
